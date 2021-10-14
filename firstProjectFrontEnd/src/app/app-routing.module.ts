d4670cfa527889346216da9a085c1a19063996f3
Yasser Abd Elghany <yasser.m22291@gmail.com>
2021-10-14 15:50:21 +0200

    Angular Routing

--- firstProjectFrontEnd/src/app/app-routing.module.ts
+++ firstProjectFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'login'  ,component: LoginComponent},
+    {path:'signup'  ,component: SignupComponent},
+    {path:'profile'  ,component: ProfileComponent},
+    {path:'request-password-reset'  ,component: RequestResetComponent},
+    {path:'response-password-reset'  ,component: ResponseResetComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
